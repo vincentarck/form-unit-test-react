9cbcbb121147ab404b9fcc6cf6c109278cdaf662
vincent <vincenthambaya@gmail.com>
2022-10-09 07:35:27 +0700

    test: wirte unit test to make sure all the components are loaded in the browser

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render login component in the document', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputNode = screen.getByTestId('form-element');
+  expect(inputNode).toBeInTheDocument();
 });
+
+test('render intro login component in the document', () => {
+  render(<App />);
+  const inputNode = screen.getByTestId('intro-form');
+  expect(inputNode).toBeInTheDocument();
+})
