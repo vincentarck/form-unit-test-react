2adf24492ba520506d8896fdecb26df504a27295
vincent <vincenthambaya@gmail.com>
2022-10-09 08:43:13 +0700

    feat: remove handleSubmit and  perform onchange input for testing purpose

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,31 +1,26 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
 export default function Form() {
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const email:string | undefined = emailRef.current?.value
-    const password:string | undefined = passwordRef.current?.value
-    if(email && password){
-      alert(`Email anda: ${email}\nPassword anda: ${password}`)
-    }else if(email || password){
-      email ? alert("Harap isi Password anda") : alert("Harap isi Email anda")
-    }
-  }
   return (
-    <main className="flex-1 flex flex-col items-center justify-center bg-slate-100 py-24 lg:py-0" data-testid="form-element">
+    <main
+      className="flex-1 flex flex-col items-center justify-center bg-slate-100 py-24 lg:py-0"
+      data-testid="form-element"
+    >
       <div className="text-left w-4/5 xl:w-[70%]">
         <h1 className="text-sky-700 font-bold text-3xl">Welcome Back</h1>
         <h2 className="text-gray-500 font-bold text-2xl">
           Sign in to continue
         </h2>
-        <form className="shadow-lg p-8 bg-white mt-10 " onSubmit={(e:React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
+        <form
+          className="shadow-lg p-8 bg-white mt-10 "
+        >
           <input
-            ref={emailRef}
             aria-label="Email"
             type="email"
+            value={email}
             className="
             block
             w-full
@@ -42,12 +37,14 @@ export default function Form() {
             m-0
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-            id="exampleFormControlInput1"
             placeholder="Email"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
           />
           <div className="mt-8 space-y-4 ">
             <input
-              ref={passwordRef}
+              value={password}
               type="password"
               className="
             block
@@ -65,8 +62,10 @@ export default function Form() {
             m-0
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-              id="exampleFormControlInput1"
               placeholder="Password"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
             />
             <p className="text-sky-700 font-semibold cursor-pointer text-sm">
               Forgot password?
@@ -79,7 +78,10 @@ export default function Form() {
                 Create account
               </p>
             </div>
-            <button className="px-5 py-3 text-white font-semibold bg-sky-700 rounded-lg text-sm lg:text-base">
+            <button
+              className="px-5 py-3 text-white font-semibold bg-sky-700 rounded-lg text-sm lg:text-base"
+              disabled={!email || !password}
+            >
               Sign In
             </button>
           </div>
